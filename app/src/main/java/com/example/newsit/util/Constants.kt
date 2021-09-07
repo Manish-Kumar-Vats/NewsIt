a739879c55fe5defa1176cfb1beecfd2a3a932eb
Sahil Makkar <sahilmakkar1411@gmail.com>
2021-09-07 21:47:44 +0530

    Error

--- app/src/main/java/com/example/newsit/util/Constants.kt
+++ app/src/main/java/com/example/newsit/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.newsit.util
 
 class Constants {
     companion object{
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "b2e31c37853b4ddb854bdc62754ea2dc"
         const val BASE_URL = "https://newsapi.org"
     }
 }
